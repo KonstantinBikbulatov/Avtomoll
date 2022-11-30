3b10182d0669c63d36343e49f2c808ccfcab10b1
Shakirov_Arthur <Wolverane32@gmail.com>
2022-11-30 09:59:47 +0500

    Fundament for SM

--- Avtomoll/Controllers/ServiceManager/ServiceManagerController.cs
+++ Avtomoll/Controllers/ServiceManager/ServiceManagerController.cs
@@ -12,16 +12,42 @@ namespace Avtomoll.Controllers.ServiceManager
         public ServiceManagerController(IRepository<ServiceHistory> repository)
         {
             Repository = repository;
-            model = Repository.GetList().Select(s => new ServiceHistoryViewModel(s));
         }
-
-        private IEnumerable<ServiceHistoryViewModel> model;
 
         private IRepository<ServiceHistory> Repository { get; set; }
 
         public IActionResult Index()
         {
-            return View(model);
+            return View(Repository.GetList().Select(s => new ServiceHistoryViewModel(s)));
         }
+
+
+        public IActionResult Edit(long id)
+        {
+            return View(new ServiceHistoryViewModel(Repository.Read(id)));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ServiceHistoryViewModel model)
+        {
+            ServiceHistory service = new ServiceHistory()
+            {
+                CarBrand = model.CarBrand,
+                NameClient = model.ClientName,
+                PhoneClient = model.ClientPhone,
+                Status = model.Status,
+                VisitTime = model.VisitTime,
+                TypeCar = model.TypeCar,
+            };
+            Repository.Update(service);
+
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult Details(long id)
+        {
+            return View(new ServiceHistoryViewModel(Repository.Read(id)));
+        }
+            
     }
 }
