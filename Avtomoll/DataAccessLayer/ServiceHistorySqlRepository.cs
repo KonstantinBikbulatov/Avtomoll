9802e7e2f58f0d0c72384fd04174a847e2f474b3
Shakirov_Arthur <Wolverane32@gmail.com>
2022-12-03 15:18:29 +0500

    new repo

---
+++ Avtomoll/DataAccessLayer/ServiceHistorySqlRepository.cs
@@ -0,0 +1,54 @@
+﻿using Avtomoll.Abstract;
+using Avtomoll.Domains;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Avtomoll.DataAccessLayer
+{
+    public class ServiceHistorySqlRepository : IRepository<ServiceHistory>
+    {
+        private readonly ApplicationDbContext context;
+        private DbSet<ServiceHistory> leads;
+
+        public ServiceHistorySqlRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+            leads = context.ServiceHistory;
+        }
+        public void Create(ServiceHistory model)
+        {
+            leads.Add(model);
+            context.SaveChanges();
+        }
+
+        public void Delete(long id)
+        {
+            var lead = Read(id);
+            leads.Remove(lead);
+            context.SaveChanges();
+        }
+
+        public ServiceHistory FindByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ServiceHistory> GetList() => leads;
+
+        public ServiceHistory Read(long id) => leads.Find(id);
+
+        public void Update(ServiceHistory model)
+        {
+            var serv = Read(model.ServiceHistoryId);
+
+            serv.CarBrand = model.CarBrand;
+            serv.NameClient = model.NameClient;
+            serv.PhoneClient = model.PhoneClient;
+            serv.Status = serv.Status;
+            serv.VisitTime = model.VisitTime;
+            serv.TypeCar = model.TypeCar;
+
+            context.SaveChanges();
+        }
+    }
+}
