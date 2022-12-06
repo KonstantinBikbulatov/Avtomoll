17899906f4dcd64a80ede049036a908c02bcfe6e
Dimo4ka <9227356873@mail.ru>
2022-12-06 21:35:02 +0500

    Незначительные изменения. Создал пустой контроллер.

---
+++ Avtomoll/Controllers/Client/ServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Avtomoll.Controllers.Client
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
