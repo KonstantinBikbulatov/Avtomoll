328a51a06251ee59e592fbf732d7d57d19c0cda3
Dimo4ka <9227356873@mail.ru>
2022-12-06 21:35:44 +0500

    Создал модель на которой будет основана View

---
+++ Avtomoll/ViewModel/ServiceOrderViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Avtomoll.Domains;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Avtomoll.ViewModel
+{
+    public class ServiceOrderViewModel
+    {
+        [HiddenInput(DisplayValue = false)]
+        public long ServiceId { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public List<SelectListItem> ListGroups { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public int GroupServiceId { get; set; }
+        public GroupService GroupService { get; set; }
+        public string GroupName { get; set; }
+
+        [Required()]
+        [Display(Name = "Выбор услуги")]
+        public string Name { get; set; }
+
+        [Required()]
+        [Display(Name = "Выбор времени")]
+        public string Time { get; set; }
+    }
+}
