015b8c6a74cb9ddf5e730dcce235347644e21f74
Shakirov_Arthur <Wolverane32@gmail.com>
2022-12-12 12:15:16 +0500

    get started

---
+++ Avtomoll/Controllers/ManagersManager/ManagersManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avtomoll.Controllers.ManagersManager
+{
+    public class ManagersManager : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public ManagersManager(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
